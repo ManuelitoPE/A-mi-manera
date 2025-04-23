13252741ce19af4e2ea0e7bba506a2a5620f13e5
Manu3 <Manu3@MANU3LIT0>
2025-04-23 01:46:49 -0500

    Pedidos para yaser

--- AMiManera/AMiManeraCapaDominioModel/src/main/java/com/restaurante/amm/amimaneracapadominiomodel/pedidos/Pedido.java
+++ AMiManera/AMiManeraCapaDominioModel/src/main/java/com/restaurante/amm/amimaneracapadominiomodel/pedidos/Pedido.java
@@ -1,40 +1,53 @@
 package com.restaurante.amm.amimaneracapadominiomodel.pedidos;
 
-import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import com.restaurante.amm.amimaneracapadominiomodel.gestionmesas.Mesa;
+import com.restaurante.amm.amimaneracapadominiomodel.personal.Mesero;
 
 public class Pedido {
     //Atributos
     private int idPedido;
-    private Timestamp fecha;
+    private Date fecha;
     private EstadoPedido estadoPedido;
     private double montoTotal;
-    private double montoDescuentoPromocion;
     private List<LineaPedido> listaLineaPedido;
-
+    private Mesa mesa;
+    private Mesero mesero;
+    
     //Contructores
-    public Pedido(){}
-    public Pedido(int idPedido, Timestamp fecha, EstadoPedido estadoPedido,
-                  double montoTotal, List<LineaPedido> listaLineaPedidos){
+    public Pedido(){
+        listaLineaPedido = new ArrayList<>();
+        mesa = new Mesa();
+        mesero = new Mesero();
+    }
+    public Pedido(int idPedido, Date fecha, EstadoPedido estadoPedido,
+                  double montoTotal, List<LineaPedido> listaLineaPedidos,
+                  Mesa mesa){
         this.idPedido = idPedido;
         this.fecha = fecha;
         this.estadoPedido = estadoPedido;
         this.montoTotal = montoTotal;
-        this.listaLineaPedido = listaLineaPedidos;
+        this.listaLineaPedido = new ArrayList<>(listaLineaPedidos);
+        this.mesa = new Mesa(mesa);
+        this.mesero = new Mesero(mesero);
     }
     public Pedido(Pedido pedido){
         idPedido = pedido.idPedido;
         fecha = pedido.fecha;
         estadoPedido = pedido.estadoPedido;
         montoTotal = pedido.montoTotal;
-        listaLineaPedido = pedido.listaLineaPedido;
+        listaLineaPedido = new ArrayList<>(pedido.listaLineaPedido);
+        mesa = new Mesa(mesa);
+        mesero = new Mesero(mesero);
     }
     
     //Setters
     public void setIdProducto(int idPedido){
         this.idPedido = idPedido;
     }
-    public void setFecha(Timestamp fecha){
+    public void setFecha(Date fecha){
         this.fecha = fecha;
     }
     public void setEstadoPedido(EstadoPedido estadoPedido){
@@ -46,15 +59,12 @@ public class Pedido {
     public void setListaLineaPedidos(List<LineaPedido> listaLineaPedido){
         this.listaLineaPedido = listaLineaPedido;
     }
-    public void setMontoDescuentoPromocion(double montoDescuentoPromocion) {
-        this.montoDescuentoPromocion = montoDescuentoPromocion;
-    }
     
     //Getters
     public int getIdProducto(){
         return idPedido;
     }
-    public Timestamp getFecha(){
+    public Date getFecha(){
         return fecha;
     }
     public EstadoPedido getEstadoPedido(){
@@ -66,7 +76,32 @@ public class Pedido {
     public List<LineaPedido> getListLineaPedidop(){
         return listaLineaPedido;
     }
-    public double getMontoDescuentoPromocion() {
-        return montoDescuentoPromocion;
+
+    /**
+     * @return the mesa
+     */
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    /**
+     * @param mesa the mesa to set
+     */
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
+
+    /**
+     * @return the mesero
+     */
+    public Mesero getMesero() {
+        return mesero;
+    }
+
+    /**
+     * @param mesero the mesero to set
+     */
+    public void setMesero(Mesero mesero) {
+        this.mesero = mesero;
     }
 }
