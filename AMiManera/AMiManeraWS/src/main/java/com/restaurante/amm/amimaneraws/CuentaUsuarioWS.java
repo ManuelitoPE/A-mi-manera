98a79c3d4a70095e7e70ba28858422f8ebe16177
italo_mijail <italomijail@gmail.com>
2025-06-20 16:59:49 -0500

    Actualizar cambios

--- AMiManera/AMiManeraWS/src/main/java/com/restaurante/amm/amimaneraws/CuentaUsuarioWS.java
+++ AMiManera/AMiManeraWS/src/main/java/com/restaurante/amm/amimaneraws/CuentaUsuarioWS.java
@@ -1,10 +1,7 @@
 package com.restaurante.amm.amimaneraws;
 
 import com.restaurante.amm.amimaneramodel.personal.CuentaUsuario;
-<<<<<<< HEAD
-=======
 import com.restaurante.amm.amimaneranegocio.bo.personal.ICuentaUsuarioBO;
->>>>>>> main
 import com.restaurante.amm.amimaneranegocio.boimpl.personal.CuentaUsuarioBOImpl;
 import jakarta.jws.WebService;
 import jakarta.jws.WebMethod;
@@ -14,26 +11,20 @@ import jakarta.jws.WebParam;
         targetNamespace = "https://servicios.amimaneraws.amm.restaurante.com")
 public class CuentaUsuarioWS {
 
-<<<<<<< HEAD
-=======
     private final ICuentaUsuarioBO cuentaUsuarioBO;
     
     public CuentaUsuarioWS(){
         this.cuentaUsuarioBO = new CuentaUsuarioBOImpl();
     }
->>>>>>> main
     @WebMethod(operationName = "login")
     public CuentaUsuario login(@WebParam(name = "nombreUsuario") String nombreUsuario,
             @WebParam(name = "contrasenia") String contrasenia) {
         return new CuentaUsuarioBOImpl().autenticar(nombreUsuario, contrasenia);
     }
-<<<<<<< HEAD
-=======
     
     @WebMethod(operationName = "encontrarRolDelUsuario")
     public int encontrarRolDelUsuario(@WebParam(name = "cuenta")CuentaUsuario cuenta){
         
         return cuentaUsuarioBO.obtenerIdUsuario(cuenta);
     }
->>>>>>> main
 }
